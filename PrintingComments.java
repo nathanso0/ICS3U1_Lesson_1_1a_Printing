84a38a10961d081eeb3f43ddeeaee3673ce6cd93
nathanso0 <nathan.so1165@gmail.com>
2024-02-08 11:47:18 -0500

    StarWarsPrintingComments.java

--- PrintingComments.java
+++ PrintingComments.java
@@ -8,13 +8,28 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
-    
-
+    System.out.print("You under");
+    System.out.println("estimate my POWER!");
     // Say hello on the same line
-    
-    
+   
+   
     // multiline comment
-    
+    // Draw a box
+System.out.println("     |      *");
+System.out.println("   * |     **");
+System.out.println("  /|/     ***");
+System.out.println("  [ [    ****");
+System.out.println("  ***    ****");
+System.out.println("*************");
+System.out.println("Don't Try it!");
+// Draw another box
+/*
+System.out.println("*************");
+System.out.println("*           *");
+System.out.println("*           *");
+System.out.println("*           *");
+System.out.println("*************");
+*/
     
 
   }
